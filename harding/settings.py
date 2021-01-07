cc2f0df4f6cd462f20bf656a84f22eb0450cbc7c
Kegan Ronholt <klronholt@mail.fhsu.edu>
2021-01-07 12:06:16 -0600

    debug set to false

--- harding/settings.py
+++ harding/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'harding-dev.herokuapp.com/']
 
 
 # Application definition
